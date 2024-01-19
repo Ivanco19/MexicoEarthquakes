3a5c0025f3cefc8b2e79567439c267ee6147c52e
FSOH <frida.ortega.hdz@gmail.com>
2024-01-18 23:58:59 -0600

    update

--- static/js/plots.js
+++ static/js/plots.js
@@ -1,6 +1,38 @@
-var rawDataURL = '../Sismos_mexico_.csv';
-var xField = 'Fecha';
-var yField = 'Magnitud';
+var rawDataURL = '../Sismos_mexico_2023.csv';
+
+
+
+d3.csv(rawDataURL).then((datos) => {
+    // Convertir la columna de fecha a objetos de fecha
+    datos.forEach((d) => {
+        const [dia, mes, año] = d.Fecha.split('/');
+        const fechaFormatoCorrecto = `${año}/${mes}/${dia}`;
+    d.Fecha = new Date(fechaFormatoCorrecto);
+    d.Magnitud = parseFloat(d.Magnitud)
+       // d.Fecha=fechaFormatoCorrecto
+    });
+  
+    // Agrupar los datos por día y obtener la magnitud máxima para cada día
+    const eventosMaximosDiarios = d3.rollups(
+      datos,
+      (v) => d3.max(v, (d) => d.Magnitud),
+      (d) => d3.timeDay(d.Fecha)
+    );
+  
+    // Convertir los resultados a un array de objetos
+    //const resultados = Array.from(eventosMaximosDiarios, ([Fecha, Magnitud]) => ({ Fecha, Magnitud }));
+        
+    const fechas = eventosMaximosDiarios.map(d=>d[0].toISOString().split('T')[0])
+    const magnitudes = eventosMaximosDiarios.map(d=>d[1])
+
+    // Mostrar los resultados
+    console.log(magnitudes);
+  });
+
+
+
+var xField = 'fechas';
+var yField = 'magnitudes';
 
 var selectorOptions = {
     buttons: [{
@@ -64,3 +96,28 @@ function prepData(rawData) {
         y: y
     }];
 }
+
+
+//________________________
+
+
+
+
+
+
+
+//separar estados de la columna referencia de loc
+
+d3.csv(rawDataURL).then((datos) => {
+    datos.forEach((d) => {
+        const [comp, estado] = d['Referencia de localizacion'].split(', ');
+        const estados = estado;
+
+        console.log(estados)}
+
+
+        )});
+
+//hacer count por estado
+
+//barplot
