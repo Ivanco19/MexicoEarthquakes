19f24c9872815aea9ab00cfa392e8349217ec551
FSOH <frida.ortega.hdz@gmail.com>
2024-01-19 17:07:42 -0600

    max magnitude by day finished

--- static/js/plots.js
+++ static/js/plots.js
@@ -1,123 +1,101 @@
 var rawDataURL = '../Sismos_mexico_2023.csv';
 
-
-
 d3.csv(rawDataURL).then((datos) => {
     // Convertir la columna de fecha a objetos de fecha
     datos.forEach((d) => {
         const [dia, mes, año] = d.Fecha.split('/');
         const fechaFormatoCorrecto = `${año}/${mes}/${dia}`;
-    d.Fecha = new Date(fechaFormatoCorrecto);
-    d.Magnitud = parseFloat(d.Magnitud)
-       // d.Fecha=fechaFormatoCorrecto
+        d.Fecha = new Date(fechaFormatoCorrecto);
+        d.Magnitud = parseFloat(d.Magnitud);
     });
-  
-    // Agrupar los datos por día y obtener la magnitud máxima para cada día
+
+    // Agrupar los datos por mes y obtener la magnitud máxima para cada mes
     const eventosMaximosDiarios = d3.rollups(
-      datos,
-      (v) => d3.max(v, (d) => d.Magnitud),
-      (d) => d3.timeDay(d.Fecha)
+        datos,
+        (v) => d3.max(v, (d) => d.Magnitud),
+        (d) => d3.timeDay(d.Fecha)
     );
-  
+
     // Convertir los resultados a un array de objetos
-    //const resultados = Array.from(eventosMaximosDiarios, ([Fecha, Magnitud]) => ({ Fecha, Magnitud }));
-        
-    const fechas = eventosMaximosDiarios.map(d=>d[0].toISOString().split('T')[0])
-    const magnitudes = eventosMaximosDiarios.map(d=>d[1])
+    const fechas = eventosMaximosDiarios.map(d => d[0]);
+    const magnitudes = eventosMaximosDiarios.map(d => d[1]);
 
-    // Mostrar los resultados
-    console.log(magnitudes);
-  });
+    const resultados = Array.from(eventosMaximosDiarios, ([fechas, magnitudes]) => ({ fechas, magnitudes }));
 
-
-
-var xField = 'fechas';
-var yField = 'magnitudes';
-
-var selectorOptions = {
-    buttons: [{
-        step: 'month',
-        stepmode: 'backward',
-        count: 1,
-        label: '1m'
-    }, {
-        step: 'month',
-        stepmode: 'backward',
-        count: 6,
-        label: '6m'
-    }, {
-        step: 'year',
-        stepmode: 'todate',
-        count: 1,
-        label: 'YTD'
-    }, {
-        step: 'year',
-        stepmode: 'backward',
-        count: 1,
-        label: '1y'
-    }, {
-        step: 'all',
-    }],
-};
-
-Plotly.d3.csv(rawDataURL, function(err, rawData) {
-    if(err) throw err;
-
-    var data = prepData(rawData);
-    var layout = {
-        title: 'Sismos en México en 2023',
-        xaxis: {
-            rangeselector: selectorOptions,
-            rangeslider: {}
-        },
-        yaxis: {
-            fixedrange: true
-        }
+    console.log(resultados)
+    
+    // Configuración del gráfico
+    var trace1 = {
+        type: "scatter",
+        mode: "lines",
+        name: 'Magnitud Máxima',
+        x: fechas,
+        y: magnitudes,
+        line: { color: '#17BECF' }
     };
 
-    Plotly.plot('graph', data, layout, {showSendToCloud: true});
+    var data = [trace1];
+
+    var layout = {
+        title: 'MAX MAGNITUDE BY DAY',
+        xaxis: {
+            autorange: true,
+            range: [fechas[0].toISOString(), fechas[fechas.length - 1].toISOString()],
+            rangeselector: {
+                buttons: [
+                    {
+                        count: 1,
+                        label: '1m',
+                        step: 'month',
+                        stepmode: 'backward'
+                    },
+                    {
+                        count: 3,
+                        label: '3m',
+                        step: 'month',
+                        stepmode: 'backward'
+                    },
+                    {
+                        count: 6,
+                        label: '6m',
+                        step: 'month',
+                        stepmode: 'backward'
+                    },
+                    { step: 'all' }
+                ]
+            },
+            rangeslider: {
+                range: [fechas[0].toISOString(), fechas[fechas.length - 1].toISOString()]
+            },
+            type: 'date'
+        },
+        yaxis: {
+            autorange: true,
+            range: [0, d3.max(magnitudes)],
+            type: 'linear'
+        }
+    };
+    
+
+    Plotly.newPlot('graph', data, layout);
+
 });
 
-function prepData(rawData) {
-    var x = [];
-    var y = [];
 
-    console.log(rawData.length)
-    console.log(rawData)
+// //________________________
 
-    rawData.forEach(function(datum, i) {
-        x.push(new Date(datum[xField]));
-        y.push((datum[yField]));
-    });
+// //separar estados de la columna referencia de localizacion
 
-    return [{
-        mode: 'lines',
-        x: x,
-        y: y
-    }];
-}
+// d3.csv(rawDataURL).then((datos) => {
+//     datos.forEach((d) => {
+//         const [comp, estado] = d['Referencia de localizacion'].split(', ');
+//         const estados = estado;
+
+//         console.log(estados)}
 
 
-//________________________
+//         )});
 
+// //hacer count por estado
 
-
-
-
-
-
-//separar estados de la columna referencia de loc
-
-d3.csv(rawDataURL).then((datos) => {
-    datos.forEach((d) => {
-        const [comp, estado] = d['Referencia de localizacion'].split(', ');
-        const estados = estado;
-
-        console.log(estados)}
-
-
-        )});
-
-//hacer count por estado
-
-//barplot
+// //barplot
